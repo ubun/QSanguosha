b14c74e8dcc372f8e3b24fdaee090ca713379fbf
ubun <ubun@foxmail.com>
2011-09-01 23:48:15 +0800

    Complete addpackage.

--- src/engine.cpp
+++ src/engine.cpp
@@ -42,6 +42,7 @@ extern "C" {
     Package *NewJoy();
     Package *NewSP();
     Package *NewYJCM();
+    Package *NewGoldSeinto();
 
     Scenario *NewGuanduScenario();
     Scenario *NewFanchengScenario();
@@ -72,6 +73,7 @@ Engine::Engine()
     addPackage(NewJoy());
     addPackage(NewSP());
     addPackage(NewYJCM());
+    addPackage(NewGoldSeinto());
 
     {
         Package *test_package = new Package("test");
