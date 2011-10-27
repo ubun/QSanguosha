e20503eb01a8e9b3ce819306aa4b435f257fdf01
Ubun Tenkei <ubun@foxmail.com>
2011-10-28 02:19:09 +0800

    Yabian Zuoci's problem.

--- src/package/shineway/green.cpp
+++ src/package/shineway/green.cpp
@@ -5,10 +5,59 @@
 #include "carditem.h"
 #include "room.h"
 
+class Yabian: public TriggerSkill{
+public:
+    Yabian():TriggerSkill("yabian"){
+        events << Damage;
+        frequency = Frequent;
+    }
+
+    virtual bool trigger(TriggerEvent , ServerPlayer *yanpeng, QVariant &data) const{
+        DamageStruct damage = data.value<DamageStruct>();
+
+        if(damage.card && damage.card->inherits("Slash") &&
+           damage.from == yanpeng && damage.to != yanpeng){
+            Room *room = yanpeng->getRoom();
+            if(room->askForSkillInvoke(yanpeng, objectName(), data)){
+                if(damage.to->getGeneralName() == "shenzhugeliang"){
+                    foreach(int card_id, damage.to->getPile("stars"))
+                        yanpeng->addToPile("stars", card_id, false);
+                }
+                //some special generals
+                QList<QString> marks;
+                marks
+                        << "@chaos" /*luanwu*/
+                        << "@nirvana" /*niepan*/
+                        << "@flame" /*yeyan*/
+                        ;
+                foreach(QString mark, marks){
+                    if(damage.to->getMark(mark)){
+                        yanpeng->gainMark(mark, damage.to->getMark(mark));
+                        damage.to->loseMark(mark, damage.to->getMark(mark));
+                    }
+                }
+                //some other marks
+                QStringList inscrire;
+                inscrire << damage.to->getGeneralName() << damage.to->getKingdom();
+                //QString myname = damage.from->getGeneralName();
+                //QString mykindom = damage.from->getKingdom();
+                if(damage.to->isAlive()){
+                    room->transfigure(damage.to, damage.from->getGeneralName(), false, false);
+                    room->setPlayerProperty(damage.to, "kingdom", damage.from->getKingdom());
+                }
+                room->transfigure(damage.from, inscrire.first(), false, false);
+                room->setPlayerProperty(damage.from, "kingdom", inscrire.last());
+            }
+        }
+        return false;
+    }
+};
+
 GreenPackage::GreenPackage()
     :Package("green")
 {
-
+    General *greenyanpeng = new General(this, "greenyanpeng", "shu");
+    greenyanpeng->addSkill(new Yabian);
 }
 
 ADD_PACKAGE(Green)
