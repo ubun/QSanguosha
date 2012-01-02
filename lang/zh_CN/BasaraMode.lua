9606f2b5e67419997d46d883255ff47c0723241f
Ubun <ubun@foxmail.com>
2012-01-02 17:41:33 +0800

    Dengai's Effect (cv:烨子, from Tianzihui Club) Synchronous

---
+++ lang/zh_CN/BasaraMode.lua
@@ -0,0 +1,10 @@
+-- translations for Basara mode
+
+return 
+{
+	["basara"] = "双暗将模式",
+	["#BasaraReveal"] = "%from 展示了他的武将，主将为 %arg，副将为 %arg2",
+	["RevealGeneral"] = "展示武将",
+	["anjiang"] = "暗将",
+	["#BasaraGeneralChosen"] = "你选择的主将为 %arg, 副将为 %arg2",
+}
